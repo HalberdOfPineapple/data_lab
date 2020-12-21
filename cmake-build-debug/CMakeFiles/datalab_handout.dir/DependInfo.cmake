37f2dddc359edcf98dc10a843cb467e3958ca8a0
MorphlingED <s1973609@ed.ac.uk>
2020-12-21 14:41:04 +0800

    Initialize the repository

---
+++ cmake-build-debug/CMakeFiles/datalab_handout.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/12639/Desktop/UoE 2020-2021/CMU_ICS/datalab-handout/bits.c" "C:/Users/12639/Desktop/UoE 2020-2021/CMU_ICS/datalab-handout/cmake-build-debug/CMakeFiles/datalab_handout.dir/bits.c.obj"
+  "C:/Users/12639/Desktop/UoE 2020-2021/CMU_ICS/datalab-handout/btest.c" "C:/Users/12639/Desktop/UoE 2020-2021/CMU_ICS/datalab-handout/cmake-build-debug/CMakeFiles/datalab_handout.dir/btest.c.obj"
+  "C:/Users/12639/Desktop/UoE 2020-2021/CMU_ICS/datalab-handout/decl.c" "C:/Users/12639/Desktop/UoE 2020-2021/CMU_ICS/datalab-handout/cmake-build-debug/CMakeFiles/datalab_handout.dir/decl.c.obj"
+  "C:/Users/12639/Desktop/UoE 2020-2021/CMU_ICS/datalab-handout/fshow.c" "C:/Users/12639/Desktop/UoE 2020-2021/CMU_ICS/datalab-handout/cmake-build-debug/CMakeFiles/datalab_handout.dir/fshow.c.obj"
+  "C:/Users/12639/Desktop/UoE 2020-2021/CMU_ICS/datalab-handout/ishow.c" "C:/Users/12639/Desktop/UoE 2020-2021/CMU_ICS/datalab-handout/cmake-build-debug/CMakeFiles/datalab_handout.dir/ishow.c.obj"
+  "C:/Users/12639/Desktop/UoE 2020-2021/CMU_ICS/datalab-handout/tests.c" "C:/Users/12639/Desktop/UoE 2020-2021/CMU_ICS/datalab-handout/cmake-build-debug/CMakeFiles/datalab_handout.dir/tests.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/datalab_handout.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/datalab_handout.dir/bits.c.obj"
+  "CMakeFiles/datalab_handout.dir/btest.c.obj"
+  "CMakeFiles/datalab_handout.dir/decl.c.obj"
+  "CMakeFiles/datalab_handout.dir/fshow.c.obj"
+  "CMakeFiles/datalab_handout.dir/ishow.c.obj"
+  "CMakeFiles/datalab_handout.dir/tests.c.obj"
+  "datalab_handout.exe"
+  "datalab_handout.exe.manifest"
+  "datalab_handout.pdb"
+  "libdatalab_handout.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/datalab_handout.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
